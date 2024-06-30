00a70e6aab0d1bb5c80f633d20c97d1b7925c523
Misael Cureño <curenosm@gmail.com>
2024-06-30 15:21:03 -0600

    Ejercicios algoritmos

---
+++ leetcode/src/solutions/largest_perimeter_triangle.rs
@@ -0,0 +1,21 @@
+use crate::Solution;
+use std::cmp::Reverse;
+
+impl Solution {
+    pub fn largest_perimeter(nums: Vec<i32>) -> i32 {
+        let mut nums = {
+            let mut nums: Vec<i32> = nums.iter().cloned().collect();
+            nums.sort_by_key(|&n| Reverse(n));
+            nums
+        };
+
+        for i in 0..nums.len() - 2 {
+            let (j, k) = (i + 1, i + 2);
+            if nums[k] + nums[j] > nums[i] {
+                return nums[i] + nums[j] + nums[k]
+            }
+        }
+        
+        return 0;
+    }
+}
--- leetcode/src/solutions/mod.rs
+++ leetcode/src/solutions/mod.rs
@@ -1,6 +1,8 @@
 pub mod count_square_sum_triplets;
 pub mod count_symmetric_integers;
 pub mod find_common_characters;
+pub mod find_perfect_numbers;
 pub mod find_the_k_beauty_of_a_number;
 pub mod find_the_k_or_of_an_array;
+pub mod largest_perimeter_triangle;
 pub mod sum_digits_in_base_k;
